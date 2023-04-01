f9f0797d872971db1759797ec2a6d02552da71c0
hnsia <44498178+hnsia@users.noreply.github.com>
2023-04-01 16:10:32 +0800

    Initialize project along with mikro-orm setup

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+  const emFork = orm.em.fork();
+  // const post = emFork.create(Post, { title: "my first post" });
+  // await emFork.persistAndFlush(post);
+
+  // const posts = await emFork.find(Post, {});
+  // console.log(posts);
+};
+
+main().catch((err) => {
+  console.log(err);
+});
