3ac48401418cb4e61817cd0a782b0521f5b79feb
hnsia <44498178+hnsia@users.noreply.github.com>
2023-04-02 01:34:39 +0800

    Add basic CRUD functionality for post

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
   const emFork = orm.em.fork();
-  // const post = emFork.create(Post, { title: "my first post" });
-  // await emFork.persistAndFlush(post);
 
-  // const posts = await emFork.find(Post, {});
-  // console.log(posts);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: emFork }),
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
